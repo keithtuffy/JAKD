9e5e52a30864e2964ea191c9dac4feea94a607d8
John <johntk86@gmail.com>
2014-02-22 20:07:41 +0000

    First Push

---
+++ src/MainScreen.java
@@ -0,0 +1,42 @@
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class MainScreen extends JFrame implements ActionListener {
+
+	
+	
+	
+	public MainScreen()
+	{
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void actionPerformed(ActionEvent arg0) {
+		
+		
+	}
+
+	
+	
+	
+	
+}
