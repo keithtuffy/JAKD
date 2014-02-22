619ccea8bd6b4866a0c0b79515ecab470c80db55
John <johntk86@gmail.com>
2014-02-22 21:04:12 +0000

    update 1

---
+++ src/JAKD/MainScreen.java
@@ -0,0 +1,44 @@
+package JAKD;
+
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class MainScreen extends JFrame implements ActionListener {
+
+	
+	
+	
+	public MainScreen()
+	{
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void actionPerformed(ActionEvent arg0) {
+		
+		
+	}
+
+	
+	
+	
+	
+}
---
+++ src/JAKD/Test.java
@@ -0,0 +1,17 @@
+package JAKD;
+
+
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		MainScreen m = new MainScreen();
+		m.setVisible(true);
+
+	}
+
+}
