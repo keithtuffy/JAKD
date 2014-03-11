052211e562ca0d642e5ab5e31c1a0f6983c33fc2
John <johntk86@gmail.com>
2014-03-11 13:54:54 +0000

    Added button img

--- src/JAKD/MainScreen.java
+++ src/JAKD/MainScreen.java
@@ -38,7 +38,8 @@ public class MainScreen extends JFrame implements ActionListener {
 		frame.add(sideButtons, BorderLayout.WEST);
 		
 		logo = new JLabel("");
-		logo.setIcon(new ImageIcon("D:\\College\\Sdev\\JAKD\\images\\logo.jpeg"));
+		logo.setIcon(new ImageIcon("src/resources/logo.jpeg"));
+		logo.setPreferredSize(new Dimension(295, 120));
 		gc.gridx = 0; // col
 		gc.gridy = 0; // row
 		gc.gridwidth = 1; // set gridwidth
@@ -60,46 +61,58 @@ public class MainScreen extends JFrame implements ActionListener {
 		sideButtons.add(spacer, gc); 
 		
 		genReport = new JButton("Genarate Report");
+		genReport.setIcon(new ImageIcon("src/resources/blueButton.png"));
 		genReport.setFont(new Font("sansserif",Font.BOLD,22));
-		genReport.setPreferredSize(new Dimension(300, 100));
+		genReport.setPreferredSize(new Dimension(280, 100));
+		genReport.setHorizontalTextPosition(JButton.CENTER);
+		genReport.setVerticalTextPosition(JButton.CENTER);
 		gc.gridx = 0; 
 		gc.gridy = 2; 
 		gc.gridwidth = 1; 
 		gc.gridheight = 1; 
-		gc.weighty = 0.5; 
+		gc.weighty = 0.2; 
 		gc.weightx = 0.0;
 		sideButtons.add(genReport, gc); 
 		
 		editUser = new JButton("Edit User");
+		editUser.setIcon(new ImageIcon("src/resources/blueButton.png"));
 		editUser.setFont(new Font("sansserif",Font.BOLD,22));
-		editUser.setPreferredSize(new Dimension(300, 100));
+		editUser.setPreferredSize(new Dimension(280, 100));
+		editUser.setHorizontalTextPosition(JButton.CENTER);
+		editUser.setVerticalTextPosition(JButton.CENTER);
 		gc.gridx = 0; 
 		gc.gridy = 3; 
 		gc.gridwidth = 1; 
 		gc.gridheight = 1; 
-		gc.weighty = 0.5; 
+		gc.weighty = 0.2; 
 		gc.weightx = 0.0;
 		sideButtons.add(editUser, gc); 
 		
 		editProd = new JButton("Edit Product");
+		editProd.setIcon(new ImageIcon("src/resources/blueButton.png"));
 		editProd.setFont(new Font("sansserif",Font.BOLD,22));
-		editProd.setPreferredSize(new Dimension(300, 100));
+		editProd.setPreferredSize(new Dimension(280, 100));
+		editProd.setHorizontalTextPosition(JButton.CENTER);
+		editProd.setVerticalTextPosition(JButton.CENTER);
 		gc.gridx = 0; 
 		gc.gridy = 4; 
 		gc.gridwidth = 1; 
 		gc.gridheight = 1; 
-		gc.weighty = 0.5; 
+		gc.weighty = 0.2; 
 		gc.weightx = 0.0;
 		sideButtons.add(editProd, gc); 
 		
 		financialManag = new JButton("Financial Managment");
+		financialManag.setIcon(new ImageIcon("src/resources/blueButton.png"));
 		financialManag.setFont(new Font("sansserif",Font.BOLD,22));
-		financialManag.setPreferredSize(new Dimension(300, 100));
+		financialManag.setPreferredSize(new Dimension(280, 100));
+		financialManag.setHorizontalTextPosition(JButton.CENTER);
+		financialManag.setVerticalTextPosition(JButton.CENTER);
 		gc.gridx = 0; 
 		gc.gridy = 5; 
 		gc.gridwidth = 1; 
 		gc.gridheight = 1; 
-		gc.weighty = 0.5; 
+		gc.weighty = 0.2; 
 		gc.weightx = 0.0;
 		sideButtons.add(financialManag, gc); 
 		
