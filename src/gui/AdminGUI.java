8ccd5c8b4c4a259db9d82e8de4fbc19e7397cde1
John <johntk86@gmail.com>
2014-03-12 17:42:47 +0000

    More changes

--- src/gui/AdminGUI.java
+++ src/gui/AdminGUI.java
@@ -20,14 +20,14 @@ public class AdminGUI extends JFrame implements ActionListener {
 	private static final int FRAME_WIDTH = 1148;
 	private static final int FRAME_HEIGHT = 827;
 	private JButton genReportBtn, editUserBtn, editProdBtn, financialManagBtn;
-	private JLabel logo, spacer, userDetails, firstName, lastName, addLine1, addLine2, addLine3, staffID, Pin, PPS;
-	
+	private JLabel logo, spacer, userDetails;
+	private JTextField forenameBx, surenamebx, line1Bx, line2Bx, Line3Bx, staffIDBx, pinBx, PPSBx;
 	
 	private JPanel  cardPanel, genReportPanel, editUserPanel, editProdPanel, financialPanel, userDetailsPanel;
 	private GridBagLayout layout = new GridBagLayout();
 	private GridBagConstraints gc = new GridBagConstraints();
 	private Color cl;
-	private Font font = new Font("Verdana", Font.ITALIC, 20);
+	private Font font = new Font("Verdana", Font.PLAIN, 20);
 	
 	public AdminGUI()
 	{
@@ -67,8 +67,6 @@ public class AdminGUI extends JFrame implements ActionListener {
 		sideButtons.add(logo, gc); 
 		
 		//space between logo and buttons, would rather use a "spacer" here, more research needed
-		
-		
 		spacer = new JLabel("");
 		
 		gc.gridx = 0; 
@@ -104,66 +102,7 @@ public class AdminGUI extends JFrame implements ActionListener {
 			sideButtons.add(sideButtonsArray[i],gc);
         }
 		
-		//Bellow is a longer way of doing what I've done above
-		
-	/*	genReportBtn.setIcon(new ImageIcon("src/resources/blueButton.png"));
-		genReportBtn.setFont(new Font("sansserif",Font.BOLD,22));
-		genReportBtn.setPreferredSize(new Dimension(280, 100));
-		genReportBtn.setHorizontalTextPosition(JButton.CENTER);
-		genReportBtn.setVerticalTextPosition(JButton.CENTER);
-		genReportBtn.addActionListener(this);
-		gc.gridx = 0; 
-		gc.gridy = 2; 
-		gc.gridwidth = 1; 
-		gc.gridheight = 1; 
-		gc.weighty = 0.2; 
-		gc.weightx = 0.0;
-		sideButtons.add(genReportBtn, gc); 
-		
-		
-		editUserBtn.setIcon(new ImageIcon("src/resources/blueButton.png"));
-		editUserBtn.setFont(new Font("sansserif",Font.BOLD,22));
-		editUserBtn.setPreferredSize(new Dimension(280, 100));
-		editUserBtn.setHorizontalTextPosition(JButton.CENTER);
-		editUserBtn.setVerticalTextPosition(JButton.CENTER);
-		editUserBtn.addActionListener(this);
-		gc.gridx = 0; 
-		gc.gridy = 3; 
-		gc.gridwidth = 1; 
-		gc.gridheight = 1; 
-		gc.weighty = 0.2; 
-		gc.weightx = 0.0;
-		sideButtons.add(editUserBtn, gc); 
-		
-		
-		editProdBtn.setIcon(new ImageIcon("src/resources/blueButton.png"));
-		editProdBtn.setFont(new Font("sansserif",Font.BOLD,22));
-		editProdBtn.setPreferredSize(new Dimension(280, 100));
-		editProdBtn.setHorizontalTextPosition(JButton.CENTER);
-		editProdBtn.setVerticalTextPosition(JButton.CENTER);
-		editProdBtn.addActionListener(this);
-		gc.gridx = 0; 
-		gc.gridy = 4; 
-		gc.gridwidth = 1; 
-		gc.gridheight = 1; 
-		gc.weighty = 0.2; 
-		gc.weightx = 0.0;
-		sideButtons.add(editProdBtn, gc); 
-		
-		
-		financialManagBtn.setIcon(new ImageIcon("src/resources/blueButton.png"));
-		financialManagBtn.setFont(new Font("sansserif",Font.BOLD,22));
-		financialManagBtn.setPreferredSize(new Dimension(280, 100));
-		financialManagBtn.setHorizontalTextPosition(JButton.CENTER);
-		financialManagBtn.setVerticalTextPosition(JButton.CENTER);
-		financialManagBtn.addActionListener(this);
-		gc.gridx = 0; 
-		gc.gridy = 5; 
-		gc.gridwidth = 1; 
-		gc.gridheight = 1; 
-		gc.weighty = 0.2; 
-		gc.weightx = 0.0;
-		sideButtons.add(financialManagBtn, gc); */
+
 		
 		//Different panels for action performed events on the side menu
         cardPanel = new JPanel();
@@ -179,6 +118,7 @@ public class AdminGUI extends JFrame implements ActionListener {
         editUserPanel.setLayout(new BorderLayout());
         userDetails = new JLabel("User Details");
         userDetails.setBorder(new EmptyBorder(10,500,0,0));
+        userDetails.setFont(font);
         
         
         editUserPanel.add(userDetails, BorderLayout.NORTH);
@@ -190,17 +130,26 @@ public class AdminGUI extends JFrame implements ActionListener {
         userDetailsPanel.setBorder(border);
        
         //Adding labels and textbox to the user details panel
-        JTextField [] userDetailBx = new JTextField[8]; 
+        JTextField [] userDetailBx = {
+        		forenameBx = new JTextField(),
+        		surenamebx = new JTextField(),
+        		line1Bx = new JTextField(),
+        		line2Bx = new JTextField(),
+        		Line3Bx = new JTextField(),
+        		staffIDBx = new JTextField(),
+        		pinBx = new JTextField(),
+        		PPSBx = new JTextField(),
+        }; 
         
         JLabel [] userDetailLb = {
-        firstName = new JLabel(" Forename"),
-        lastName = new JLabel(" Surename"),
-        addLine1 = new JLabel(" Line 1"),
-        addLine2 = new JLabel(" Line 2"),
-        addLine3 = new JLabel(" Line 3"),
-        staffID = new JLabel(" Staff ID"),
-        Pin = new JLabel(" Pin"),
-        PPS = new JLabel(" PPS")
+        new JLabel(" Forename"),
+        new JLabel(" Surename"),
+        new JLabel(" Line 1"),
+        new JLabel(" Line 2"),
+        new JLabel(" Line 3"),
+        new JLabel(" Staff ID"),
+        new JLabel(" Pin"),
+        new JLabel(" PPS")
         };
         for(int i = 0; i < userDetailLb.length; i++)
         {
@@ -208,7 +157,7 @@ public class AdminGUI extends JFrame implements ActionListener {
     		gc.gridy = i; 
     		gc.gridwidth = 1; 
     		gc.gridheight = 1; 
-    		gc.weighty = 20.0; 
+    		gc.weighty = 0.1; 
     		gc.weightx = 10.0;
     		gc.anchor = GridBagConstraints.WEST;
     		userDetailLb[i].setFont(font);
@@ -218,9 +167,10 @@ public class AdminGUI extends JFrame implements ActionListener {
     		gc.gridy = i  ; 
     		gc.gridwidth = 1; 
     		gc.gridheight = 1; 
-    		gc.weighty = 10.0; 
+    		gc.weighty = 0.2; 
     		gc.weightx = 10.0;
-    		userDetailBx[i] = new JTextField(20);
+//    		userDetailBx[i] = new JTextField();
+    		userDetailBx[i].setPreferredSize(new Dimension(350, 30));
         	userDetailsPanel.add(userDetailBx[i], gc);
         }
         
