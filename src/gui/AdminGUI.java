7224e6c8f65487d0fed89b69f87f081660ab35d3
John <johntk86@gmail.com>
2014-03-12 18:12:38 +0000

    Edit user buttons added

--- src/gui/AdminGUI.java
+++ src/gui/AdminGUI.java
@@ -19,11 +19,10 @@ public class AdminGUI extends JFrame implements ActionListener {
 	private JFrame frame;
 	private static final int FRAME_WIDTH = 1148;
 	private static final int FRAME_HEIGHT = 827;
-	private JButton genReportBtn, editUserBtn, editProdBtn, financialManagBtn;
+	private JButton genReportBtn, editUserBtn, editProdBtn, financialManagBtn, addUser, editUser, removeUser;
 	private JLabel logo, spacer, userDetails;
 	private JTextField forenameBx, surenamebx, line1Bx, line2Bx, Line3Bx, staffIDBx, pinBx, PPSBx;
-	
-	private JPanel  cardPanel, genReportPanel, editUserPanel, editProdPanel, financialPanel, userDetailsPanel;
+	private JPanel  cardPanel, editUserBtnsPanel, genReportPanel, editUserPanel, editProdPanel, financialPanel, userDetailsPanel;
 	private GridBagLayout layout = new GridBagLayout();
 	private GridBagConstraints gc = new GridBagConstraints();
 	private Color cl;
@@ -104,7 +103,7 @@ public class AdminGUI extends JFrame implements ActionListener {
 		
 
 		
-		//Different panels for action performed events on the side menu
+		//Different panels for action performed events on the side buttons
         cardPanel = new JPanel();
         
         genReportPanel = new JPanel();
@@ -176,6 +175,35 @@ public class AdminGUI extends JFrame implements ActionListener {
         
         editUserPanel.add(userDetailsPanel, BorderLayout.EAST);
         
+        editUserBtnsPanel = new JPanel();
+        editUserBtnsPanel.setLayout(new GridBagLayout());
+        editUserBtnsPanel.setPreferredSize(new Dimension(250, 100));
+        
+        
+        JButton [] editUserBtnsArray = {
+				addUser = new JButton("Add User"),
+				removeUser = new JButton("Remove User"),
+				editUser = new JButton("Edit User"),
+				
+		        };
+		
+		for(int i = 0; i < editUserBtnsArray.length; i++)
+        {
+			gc.gridx = 0; 
+			gc.gridy = i; 
+			gc.gridwidth = 1; 
+			gc.gridheight = 1; 
+			gc.weighty = 0.2; 
+			gc.weightx = 0.0;
+			editUserBtnsArray[i].setIcon(new ImageIcon("src/resources/blueButton.png"));
+			editUserBtnsArray[i].setFont(new Font("sansserif",Font.BOLD,12));
+			editUserBtnsArray[i].setPreferredSize(new Dimension(180, 50));
+			editUserBtnsArray[i].setHorizontalTextPosition(JButton.CENTER);
+			editUserBtnsArray[i].setVerticalTextPosition(JButton.CENTER);
+			editUserBtnsArray[i].addActionListener(this);
+			editUserBtnsPanel.add(editUserBtnsArray[i],gc);
+        }
+        editUserPanel.add(editUserBtnsPanel, BorderLayout.WEST);
         
         editProdPanel =  new JPanel();
         editProdPanel.setBackground(Color.RED);
@@ -233,6 +261,7 @@ public class AdminGUI extends JFrame implements ActionListener {
 			}*/
 			
 			cards.show(cardPanel, "second"); 
+			forenameBx.setText("cool");
         }
 		else if(e.getSource() == editProdBtn)
         {
